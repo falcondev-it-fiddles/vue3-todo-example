5916c16e478406fe7446cf57f486ad1fe848c3fc
Julian Ullrich <julian@ullrichmail.net>
2021-08-25 16:00:33 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <h1>Welcome to Your Vue.js App</h1>
+  <List :todos="todos" />
+  <div>
+    <input type="text" placeholder="neues Todo" v-model="newTodoText" />
+    <input type="button" value="Adden" @click="addTodo" />
+  </div>
 </template>
+
+<script>
+import { ref } from "vue";
+import List from "./components/List.vue";
+
+export default {
+  components: { List },
+  setup() {
+    const todos = ref(JSON.parse(localStorage.getItem("todoList")) ?? []);
+    const newTodoText = ref();
+
+    const addTodo = () => {
+      todos.value.push(newTodoText.value);
+      localStorage.setItem("todoList", JSON.stringify(todos.value));
+      newTodoText.value = "";
+    };
+
+    return { todos, addTodo, newTodoText };
+  },
+};
+</script>
---
+++ src/components/List.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul>
+    <li v-for="item in todos" :key="item">{{ item }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "List",
+  props: {
+    todos: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
